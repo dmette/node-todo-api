7f2296a8a1de71dfb6c84467061a166c579590d4
David <dave.mette@gmail.com>
2018-04-30 21:30:51 -0400

    refactor and post todos endpoint

---
+++ server/models/todo.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose');
+
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+var newTodo = new Todo({
+  text: 'address emails'
+});
+
+newTodo.save().then((doc) => {
+  console.log('Saved doc:', doc);
+}, (err) => {
+
+console.log('Unable to save');
+});
+
+var otherTodo = new Todo({
+  text: '     become a millionaire    ',
+  completed: false,
+});
+
+otherTodo.save().then((doc) => {
+  console.log(JSON.stringify(doc, undefined, 2));
+}, (err) => {
+
+console.log('Unable to save');
+});
+
+module.exports = {Todo};
---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var newUser = new User({
+  email: '     steve@gmail.com    '
+});
+
+newUser.save().then((doc) => {
+  console.log('Saved doc:', doc);
+}, (err) => {
+
+console.log('Unable to save', err);
+});
+
+module.exports = {User};
